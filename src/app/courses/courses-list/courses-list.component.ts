a2663dc4d8045aea89813f3568a56d82bd91787e
truelanz <devalanoliveira@gmail.com>
2023-06-05 17:35:19 -0300

    component courses-list and more refactoring

---
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss']
+})
+export class CoursesListComponent {
+
+@Input()
+courses: Course[] = []
+readonly displayedColumns = ['name', 'category', 'actions'];
+
+constructor(
+  private router: Router,
+  private route: ActivatedRoute) {
+
+}
+
+onAdd() {
+  console.log('onAdd')
+  this.router.navigate(['new'], {relativeTo: this.route}); //Com ActivatedRoute, criará uma rota relativa a atual.
+}
+
+}
